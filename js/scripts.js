2c9afae1b32e7490be5bb0133a0eab163871d1ee
Rony Nasr and Sebastian Prokuski <sebastian@>
2016-06-01 16:19:46 -0700

    finalize the webpage

--- js/scripts.js
+++ js/scripts.js
@@ -7,19 +7,27 @@
 $(document).ready(function(){
   $("form#survey").submit(function(event) {
   var flavor = $("input:radio[name=flavor]:checked").val();
-
+  var name = $("#name").val();
+  var animal;
   $("#bear").hide();
   $("#cat").hide();
   $("#dolphin").hide();
+  $(".results").hide();
 
   if(flavor === "chocolate") {
   $("#bear").show();
+  animal=" bear"
 } else if(flavor === "vanilla") {
   $("#dolphin").show();
+  animal=" dolphin"
 } else {
   $("#cat").show();
+  animal=" cat"
 }
+$(".name").text(name);
+$(".animal").text(animal);
 
+$(".results").show();
   event.preventDefault();
   });
 })
